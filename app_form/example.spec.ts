07228cdddbd50b6ce3804197a3e2e1dcb93dae60
AbyssalWhale <olegenikus@gmail.com>
2024-04-23 17:36:48 +0200

    pom and packages update
    - adding faker https://fakerjs.dev/
    - updating pom

--- app_form/example.spec.ts
+++ app_form/example.spec.ts
@@ -1,18 +1,39 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/home_page';
+import { faker } from '@faker-js/faker';
 
-let home_page: HomePage;
+
+let homePage: HomePage;
+let firstName: string;
+let lastName: string;
+let email: string;
+let password: string;
+let passwordConfirm: string;
 
 test.beforeEach(async ({ page }) => {
-  //await page.goto('https://qa-task.redvike.rocks/')
-  home_page = new HomePage(page)
-  await home_page.navigate()
+  firstName = faker.person.firstName();
+  lastName = faker.person.lastName();
+  email = faker.internet.email();
+  password = faker.internet.password();
+  passwordConfirm = password;
+
+  homePage = new HomePage(page)
+  await homePage.navigate()
 });
 
 test.afterAll(async() => {
-  home_page.closePage()
+  homePage.closePage()
 })
 
 test('has title', async ({ page }) => {
-  await expect(page).toHaveTitle('Recruitment Task - Web Form');
+  const title_current = await homePage.getTitle() 
+  await expect(title_current).toBe('Recruitment Task - Web Form');
+
+  await homePage.inputFirstName(firstName)
+  await homePage.inputLastName(lastName)
+  await homePage.inputEmail(email)
+  await homePage.inputPassword(password)
+  await homePage.inputConfirmPassword(passwordConfirm)
+  await homePage.unlockSlider()
+  await homePage.clickSubmit()
 });
